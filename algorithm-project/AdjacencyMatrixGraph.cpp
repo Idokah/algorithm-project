79e1acff4d73fe02b022d7da660c23fb6451d05e
Romi Erez <romierez04@gmail.com>
2021-04-16 13:38:23 +0300

    AdjacencyMatrixGraph fix

--- algorithm-project/AdjacencyMatrixGraph.cpp
+++ algorithm-project/AdjacencyMatrixGraph.cpp
@@ -1,78 +1,80 @@
 #include "AdjacencyMatrixGraph.h"
-#include <fstream>
 
-int NO_EDGE = -1; // We are assumming all the weight in the graph is not negative
+int NO_EDGE = -1; // We are assuming all the weight in the graph is not negative
 
-class AdjacencyMatrixGraph 
+
+// Initialize the matrix to zero
+AdjacencyMatrixGraph::AdjacencyMatrixGraph(int numVertices)
 {
-private:
-    int** adjMatrix;
-    int numVertices;
+    makeEmptyGraph(numVertices);
+}
 
-public:
-    // Initialize the matrix to zero
-    AdjacencyMatrixGraph(int numVertices) 
+void AdjacencyMatrixGraph::makeEmptyGraph(int numVertices)
+{
+    this->numVertices = numVertices;
+    adjMatrix = new int*[numVertices];
+    for (int i = 0; i < numVertices; i++)
     {
-        makeEmptyGraph(numVertices);
+        adjMatrix[i] = new int[numVertices];
+        for (int j = 0; j < numVertices; j++)
+            adjMatrix[i][j] = NO_EDGE;
     }
+}
 
-    void makeEmptyGraph(int numVertices)
+bool AdjacencyMatrixGraph::isAdjacent(int u, int v)
+{
+    return (adjMatrix[u][v] != NO_EDGE);
+}
+
+// Read edges from file
+void AdjacencyMatrixGraph::load(istream& in)
+{
+    string line;
+    int i, j, weight;
+    while (!in.eof( ))
     {
-        this->numVertices = numVertices;
-        adjMatrix = new int*[numVertices];
-        for (int i = 0; i < numVertices; i++) 
-        {
-            adjMatrix[i] = new int[numVertices];
-            for (int j = 0; j < numVertices; j++)
-                adjMatrix[i][j] = NO_EDGE;
-        }
+        in >> i >> j >> weight;
+        addEdge(i, j, weight);
     }
+}
 
-    bool isAdjacent(int u, int v)
+// Add edges
+void AdjacencyMatrixGraph::addEdge(int i, int j, int c)
+{
+    adjMatrix[i][j] = c;
+}
+
+// Remove edges
+void AdjacencyMatrixGraph::removeEdge(int i, int j)
+{
+    adjMatrix[i][j] = NO_EDGE;
+}
+
+// Print the martix
+void AdjacencyMatrixGraph::toString()
+{
+    for (int i = 0; i < numVertices; i++)
     {
-        return (adjMatrix[u][v] != NO_EDGE);
+        cout << i << " : ";
+        for (int j = 0; j < numVertices; j++)
+            cout << adjMatrix[i][j] << " ";
+        cout << endl;
     }
+}
 
-
-    void load(istream& in) 
-    {
-        string line;
-        int i, j, weight;
-        while (!in.eof( ))
-        {
-            in >> i >> j >> weight;
-            addEdge(i, j, weight);
-        }
+LinkedList AdjacencyMatrixGraph::getAdjList(int u)
+{
+    LinkedList lst;
+    for (int i = 0; i < numVertices; i++) {
+        if (isAdjacent(u, i))
+            lst.addNode(i, adjMatrix[u][i]);
     }
+    return lst;
+}
 
-    // Add edges
-    void addEdge(int i, int j, int c) 
-    {
-        adjMatrix[i][j] = c;
-    }
-
-    // Remove edges
-    void removeEdge(int i, int j) 
-    {
-        adjMatrix[i][j] = NO_EDGE;
-    }
-
-    // Print the martix
-    void toString() 
-    {
-        for (int i = 0; i < numVertices; i++) 
-        {
-            cout << i << " : ";
-            for (int j = 0; j < numVertices; j++)
-                cout << adjMatrix[i][j] << " ";
-            cout << endl;
-        }
-    }
-
-    ~AdjacencyMatrixGraph() 
-    {
-        for (int i = 0; i < numVertices; i++)
-            delete[] adjMatrix[i];
-        delete[] adjMatrix;
-    }
-};
+AdjacencyMatrixGraph::~AdjacencyMatrixGraph()
+{
+    for (int i = 0; i < numVertices; i++)
+        delete[] adjMatrix[i];
+    delete[] adjMatrix;
+}
--- algorithm-project/AdjacencyMatrixGraph.h
+++ algorithm-project/AdjacencyMatrixGraph.h
@@ -2,6 +2,27 @@
 #define ALGORITHM_PROJECT_ADJACENCYMATRIXGRAPH_H
 
 #include <iostream>
+#include "LinkedList.h"
+#include <fstream>
+
 using namespace std;
+
+class AdjacencyMatrixGraph
+{
+private:
+    int** adjMatrix;
+    int numVertices;
+
+public:
+    AdjacencyMatrixGraph(int numVertices);
+    void makeEmptyGraph(int numVertices);
+    bool isAdjacent(int u, int v);
+    void load(istream& in);
+    void addEdge(int i, int j, int c);
+    void removeEdge(int i, int j);
+    void toString();
+    LinkedList getAdjList(int u);
+    ~AdjacencyMatrixGraph();
+};
 
 #endif //ALGORITHM_PROJECT_ADJACENCYMATRIXGRAPH_H
