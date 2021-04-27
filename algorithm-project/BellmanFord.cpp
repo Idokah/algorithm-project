66af141388cd64b68dbaa3ae929d4849fdccdbc5
Ido <idokk96@gmail.com>
2021-04-27 19:18:46 +0300

    split BellmanFord to header and cpp file

---
+++ algorithm-project/BellmanFord.cpp
@@ -0,0 +1,54 @@
+#include "BellmanFord.h"
+
+float BellmanFord::bellmanFord(Graph* G, int s, int t) {
+    int u, v;
+    Edge* edge;
+    LinkedList* adjList = G->getAllEdges();
+    int n = G->getNumOfVertex();
+    float* d = new float[n];
+    int* p = new int[n];
+    init(s, d, p, n);
+    for (int i = 0; i < n - 1; ++i) {
+        edge = adjList->getHead();
+        while (edge != nullptr) {
+            v = edge->dstVertex - 1;
+            u = edge->srcVertex - 1;
+            if (isImprovingEdge(u, v, edge->weight, d)) {
+                d[v] = d[u] + edge->weight;
+                p[v] = u;
+            }
+            edge = edge->next;
+        }
+    }
+
+    edge = adjList->getHead();
+    while (edge != nullptr)
+    {
+        v = edge->dstVertex - 1;
+        u = edge->srcVertex - 1;
+        if (isImprovingEdge(u, v, edge->weight, d))
+            throw invalid_argument("Negative Cycle");
+        edge = edge->next;
+    }
+
+    float distanceToT = d[t - 1];
+    delete[] d;
+    delete[] p;
+    delete adjList;
+    return distanceToT;
+}
+
+bool BellmanFord::isImprovingEdge(int u, int v, float weight, float* d)
+{
+    return (d[v] == -1 || (d[v] > d[u] + weight));
+}
+
+void BellmanFord::init(int s, float* d, int* p, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        d[i] = -1;
+        p[i] = -1;
+    }
+    d[s - 1] = 0;
+}
--- algorithm-project/BellmanFord.h
+++ algorithm-project/BellmanFord.h
@@ -6,54 +6,9 @@
 static class BellmanFord
 {
 public:
-	static int bellmanFord(Graph* G, int s ,int t) {
-        int u, v;
-        Edge *edge;
-        LinkedList *adjList = G->getAllEdges();
-        int n = G->getNumOfVertex();
-        float *d = new float[n];
-        int *p = new int[n];
-        init(s, d, p, n);
-        for (int i = 0; i < n - 1; ++i) {
-            edge = adjList->getHead();
-            while (edge != nullptr) {
-                v = edge->dstVertex - 1;
-                u = edge->srcVertex - 1;
-                if (isImprovingEdge(u, v, edge->weight, d)) {
-                    d[v] = d[u] + edge->weight;
-                    p[v] = u;
-                }
-                edge = edge->next;
-            }
-        }
-
-        edge = adjList->getHead();
-        while (edge != nullptr)
-        {
-            v = edge->dstVertex - 1;
-            u = edge->srcVertex - 1;
-            if (isImprovingEdge(u, v, edge->weight, d))
-                throw invalid_argument("Negative Cycle");
-            edge = edge->next;
-        }
-
-        int distanceToT = d[t-1];
-        delete[] d;
-        delete[] p;
-        delete adjList;
-        return distanceToT;
-	}
+	static float bellmanFord(Graph* G, int s, int t);
 private:
-    static bool isImprovingEdge(int u, int v, float weight, float* d){
-	    return (d[v] == -1 || (d[v] > d[u] + weight));
-	}
-	static void init(int s, float* d, int* p, int n) 
-	{
-		for (int i = 0; i < n; ++i) 
-		{
-			d[i] = -1;
-			p[i] = -1;
-		}
-		d[s-1] = 0;
-	}
+	static bool isImprovingEdge(int u, int v, float weight, float* d);
+	static void init(int s, float* d, int* p, int n);
+	
 };
